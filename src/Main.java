6c9a5623afc3bc371728b69f9aaec4ded9c2e3a0
SAYILXON <sayilxon8@gmail.com>
2026-03-03 15:42:49 +0500

    dars

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+class Student {
+    String name;
+    int age;
+
+    Student(String name){
+         this(name ,18);
+    }
+    Student(String name, int age) {
+        this.name = name;
+        this.age = age;;
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/dars.java
@@ -0,0 +1,12 @@
+
+public class dars {
+
+    public static void main(String[] args) {
+        System.out.println(2+3*4);
+    }
+
+    public static int km(int m) {
+        return(m * 100);
+    }
+
+}
